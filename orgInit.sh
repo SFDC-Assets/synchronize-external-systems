5916fc06902c0bf32179a620eebe38d03fd36ba5
Jonathan Zhou <34146693+zhou059@users.noreply.github.com>
2021-09-01 16:37:05 -0700

    Update orgInit.sh

--- orgInit.sh
+++ orgInit.sh
@@ -1,4 +1,4 @@
-sfdx force:org:create -f config/project-scratch-def.json -d 30 -s
+sfdx force:org:create -f config/project-scratch-def.json -d 3 -s
 sfdx force:source:push
 sfdx force:user:permset:assign -n demoPerms
 sfdx automig:load -d data
